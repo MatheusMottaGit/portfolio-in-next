2dc6d983204f8b1588a2c6d5b11bfcc2fb66b3db
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-05 19:25:54 -0300

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,15 +1,19 @@
 import React from "react";
 
-export interface GithubContext {
-  repositories: Repo[] | undefined
+export interface Repo {
+  id: number
+  name: string
+  created_at: string
+}
+
+export interface Profile {
+  name: string
+  followers: number
+  public_repos: number
+  avatar_url: string
 }
 
 export interface Children {
   children: React.ReactNode
 }
 
-export interface Repo {
-  id: number
-  name: string
-  created_at: string
-}
