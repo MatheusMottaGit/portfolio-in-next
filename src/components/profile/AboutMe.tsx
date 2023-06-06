6821cc094891bd98479ae8f32903a05b9bdb6e13
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-06 12:32:30 -0300

    about-profile

---
+++ src/components/profile/AboutMe.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <section className='bg-profile rounded-lg w-3/5 mt-12'>
+      dbhwfjh
+    </section>
+  )
+}
+
+export default AboutMe
--- src/components/profile/Preview.tsx
+++ src/components/profile/Preview.tsx
@@ -1,14 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Preview = () => {
+
+  const preview = 'Olá! Tenho 19 anos e por meio desse website, tenho o objetivo de receber a oportunidade  de conhecer o ambiente profissional de desenvolvimento, tanto na parte técnica (programação), quanto na parte das relações interpessoais, e com isso, amadurecer como programador e pessoa. Quero por meio dessa autodivulgação,  me posicionar no mercado e registrar minha trajetória!'
+
   return (
-    <div className='w-full flex flex-col gap-1 text-justify text-text text-xs leading-5'>
-      Olá! Tenho 19 anos e por meio desse website, tenho o objetivo de receber a oportunidade 
-      de conhecer o ambiente
-      profissional de desenvolvimento, tanto na parte técnica
-      (programação), quanto na parte das relações interpessoais,
-      e com isso, amadurecer como programador e pessoa. Quero por meio dessa autodivulgação, 
-      me posicionar no mercado e registrar minha trajetória!
+    <div className='w-full gap-1 text-justify text-text text-xs leading-5'>
+      {preview}
+
+      <Link 
+        className='text-blue underline font-bold ml-2' 
+        href={'/aboutMe'}
+      >
+        Ver mais
+      </Link>
     </div>
   )
 }
