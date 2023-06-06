8bd6d5296fdc3fbcef28548882d386c8d463322b
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-06 14:47:37 -0300

    about-profile

--- src/components/profile/AboutMe.tsx
+++ src/components/profile/AboutMe.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <section className='bg-profile rounded-lg w-3/5 mt-12'>
-      dbhwfjh
+    <section className='w-3/5 mt-12 p-4 flex flex-col items-center gap-3 text-center animate-slideIn'>
+      <h1 className='text-xl text-title/80 font-bold ml-3'>Sobre mim</h1>
+
+      <p className='text-text/50 text-sm'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti architecto dolorum, commodi vitae enim iure consectetur cum incidunt quas asperiores vel ipsum doloribus soluta molestias recusandae velit fugiat. Nesciunt, debitis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus voluptate pariatur dolorem dolores ratione. Error at voluptate praesentium dignissimos, nostrum rem quam autem commodi, suscipit ullam enim dolores tempore molestiae.
+      </p>
     </section>
   )
 }
--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -3,26 +3,26 @@ import React from 'react'
 import axios from 'axios'
 
 import MyLocation from '../location/MyLocation'
-// import Avatar from './Avatar'
 import NameAndLinkToGithub from './NameAndLinkToGithub'
 import Preview from './Preview'
+import Avatar from './Avatar'
 
-// import { profile } from '@/utils/enviroment'
-// import { Profile } from '../../types/types'
+import { profile } from '@/utils/enviroment'
+import { Profile } from '../../types/types'
 
 const Profile = async () => {
 
-  // const response = await axios.get(profile)
-  // const profileData: Profile = await response.data
+  const response = await axios.get(profile)
+  const profileData: Profile = await response.data
 
   return (
-    <div className='w-3/5 h-[200px] rounded-xl bg-profile flex justify-center gap-4 items-center p-4 mt-6 animate-slideIn'>
-      {/* <Avatar 
+    <div className='w-3/5 h-fit rounded-xl bg-profile flex justify-center gap-4 items-center p-4 mt-6 animate-slideIn max-[1119px]:flex-col'>
+      <Avatar 
         avatar_url={profileData.avatar_url} 
         name={''} 
         followers={0} 
         public_repos={0} 
-      /> */}
+      />
 
       <div className='h-full w-full flex flex-col gap-3 bg-base rounded-lg p-4'>
         <NameAndLinkToGithub />
