93c5fb8180577e1d0782451cb4529edf31314eae
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-06 11:06:57 -0300

    app-pages

--- src/app/aboutMe/page.tsx
+++ src/app/aboutMe/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Timeline from '@/components/Timeline'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='relative w-3/5 h-fit mt-12 rounded-lg animate-slideIn'>
+      <span className='absolute bg-profile/60 h-full p-[1px] rounded-full'></span>
+
+      <div className='relative flex flex-col gap-4 left-4 pr-5'>
+        <Timeline />
+      </div>
+    </section>
   )
 }
 
